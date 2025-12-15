fe244cd2f3752bf29fd7e9e03e3cd2171e3a7b1b
PEGASUS Dev Team <dev@pegasus.com>
2025-12-15 14:21:34 -0300

    fix: corrigir URL da API no service de solicitacoes - adicionar /api no baseURL

--- src/services/solicitacoesService.ts
+++ src/services/solicitacoesService.ts
@@ -18,7 +18,7 @@ class SolicitacoesService {
 
   constructor() {
     this.api = axios.create({
-      baseURL: `${API_URL}/solicitacoes`,
+      baseURL: `${API_URL}/api/solicitacoes`,
       headers: {
         'Content-Type': 'application/json',
       },
